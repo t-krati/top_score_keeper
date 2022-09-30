160e09a68a5df0375a5e89b9b8a23b1a9f2ebe9f
Krati Tiwari <tiwari.krati96@gmail.com>
2022-09-30 17:35:52 +0530

    initial commit

---
+++ src/main/java/com/krati/top_score_keeper/TopScoreKeeperApplication.java
@@ -0,0 +1,13 @@
+package com.krati.top_score_keeper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TopScoreKeeperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TopScoreKeeperApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/krati/top_score_keeper/TopScoreKeeperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.krati.top_score_keeper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TopScoreKeeperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
